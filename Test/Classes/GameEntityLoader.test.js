441b5d625a4ad5095a71b02d417b5b65866cd371
Samantha Divina <lavcorps@protonmail.com>
2026-02-04 13:53:32 -0700

    fix GEL tests

--- Test/Classes/GameEntityLoader.test.js
+++ Test/Classes/GameEntityLoader.test.js
@@ -841,7 +841,7 @@ describe('GameEntityLoader test', () => {
                     "Error: Couldn't load player on row 7. The stamina stat given is not an integer.",
                     "Error: Couldn't load player on row 8. \"invalid\" is not a room.",
                     "Error: Couldn't load player on row 9. \"invalid\" is not a status effect.",
-                    "Error: Couldn't load player on row 10. The given representation of the time remaining for the status \"feminizing\" is not valid.",
+                    "Error: Couldn't load player on row 10. \"365\" is not a valid representation of the time remaining for the status \"feminizing\".",
                     "Error: Couldn't load player on row 11. The Discord ID for an NPC must be a URL with a .jpg, .jpeg, .png, .webp, or .avif extension.",
                 ];
                 expect(errors).not.toEqual([]);

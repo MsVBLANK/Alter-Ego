d61f62ede7d4ab5f6ede91d77b7310bb2040bb2d
Samantha Divina <lavcorps@protonmail.com>
2026-02-06 16:12:04 -0700

    port legacy unstash tests

--- Test/Data/Actions/UnstashAction.test.js
+++ Test/Data/Actions/UnstashAction.test.js
@@ -1,3 +1,58 @@
+import UnstashAction from '../../../Data/Actions/UnstashAction.js';
+import { clearQueue } from '../../../Modules/messageHandler.js';
+import { createMockMessage } from '../../__mocks__/libs/discord.js';
+
 describe('UnstashAction test', () => {
-    test('', () => {});
+    beforeAll(async () => {
+        if (!game.inProgress) await game.entityLoader.loadAll();
+    });
+    
+    afterAll(async () => {
+        clearQueue(game);
+        vi.resetAllMocks();
+        await game.entityLoader.loadPlayers(false);
+    });
+    
+    test('ported legacy test', async () => {
+        const vivian = game.entityFinder.getLivingPlayer("Vivian");
+        const hand = game.entityFinder.getPlayerFreeHand(vivian);
+        let toiletPaperPack = game.entityFinder.getInventoryItem("PACK OF TOILET PAPER 2", "Vivian", "WHITE JEANS 2/RIGHT POCKET", "BAG");
+        const quiver = game.entityFinder.getInventoryItem("VIVIANS QUIVER", "Vivian")
+        const quiverWeightBefore = quiver.weight;
+        const unstashAction = new UnstashAction(game, createMockMessage(), vivian, vivian.location, false);
+        unstashAction.performUnstash(toiletPaperPack, hand, toiletPaperPack.container, toiletPaperPack.container.inventory.get(toiletPaperPack.slot));
+        
+        // Test that all of the data was converted properly.
+        toiletPaperPack = hand.equippedItem;
+        expect(toiletPaperPack).not.toBeNull();
+        expect(quiver.weight).toStrictEqual(quiverWeightBefore - toiletPaperPack.weight);
+        const buns = toiletPaperPack.inventory.get("PACK").items[0];
+        expect(toiletPaperPack.name).toStrictEqual("PACK OF TOILET PAPER");
+        expect(buns.name).toStrictEqual("HAMBURGER BUN");
+        expect(toiletPaperPack.pluralName).toStrictEqual("PACKS OF TOILET PAPER");
+        expect(buns.pluralName).toStrictEqual("HAMBURGER BUNS");
+        expect(toiletPaperPack.singleContainingPhrase).toStrictEqual("a PACK OF TOILET PAPER");
+        expect(buns.singleContainingPhrase).toStrictEqual("a HAMBURGER BUN");
+        expect(toiletPaperPack.pluralContainingPhrase).toStrictEqual("PACKS OF TOILET PAPER");
+        expect(buns.pluralContainingPhrase).toStrictEqual("HAMBURGER BUNS");
+        expect(toiletPaperPack.equipmentSlot).toStrictEqual(hand.id);
+        expect(buns.equipmentSlot).toStrictEqual(hand.id);
+        expect(toiletPaperPack.containerName).toStrictEqual("");
+        expect(buns.containerName).toStrictEqual("PACK OF TOILET PAPER 2/PACK");
+        expect(toiletPaperPack.container).toBeNull();
+        expect(buns.container.name).toStrictEqual("PACK OF TOILET PAPER");
+        expect(toiletPaperPack.slot).toStrictEqual("");
+        expect(buns.slot).toStrictEqual("PACK");
+        expect(toiletPaperPack.quantity).toStrictEqual(1);
+        expect(buns.quantity).toStrictEqual(12);
+        expect(isNaN(toiletPaperPack.uses));
+        expect(buns.uses).toStrictEqual(1);
+        expect(toiletPaperPack.weight).toStrictEqual(buns.weight * buns.quantity + toiletPaperPack.prefab.weight);
+        expect(buns.weight).toStrictEqual(1);
+        expect(toiletPaperPack.inventory.size).toStrictEqual(1);
+        expect(buns.inventory.size).toStrictEqual(0);
+        expect(toiletPaperPack.row).toStrictEqual(18);
+        expect(buns.row).toStrictEqual(33);
+        
+    });
 });

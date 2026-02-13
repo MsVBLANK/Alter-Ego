80d50dbc45cacd1128af3dcf3d6c516ecd1d5915
Molly <alteregomolly@pm.me>
2026-02-13 14:22:24 -0500

    Fixed attachments not appearing in spectate channels.

--- Modules/discordUtils.js
+++ Modules/discordUtils.js
@@ -46,7 +46,7 @@ export function generateWebhookMessageDisplayCreateOptions(messageDisplayType, g
         components: messageDisplayType === MessageDisplayType.PLAIN_TEXT ? [] : createNarrateComponents(messageDisplayType, game, messageText, player, files),
         flags: generateFlags(messageDisplayType),
         embeds: messageDisplayType === MessageDisplayType.PLAIN_TEXT ? embeds : [],
-        files: messageDisplayType === MessageDisplayType.PLAIN_TEXT ? [] : files
+        files: messageDisplayType === MessageDisplayType.PLAIN_TEXT ? files : []
     };
 }
 

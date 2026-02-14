fb3ede8f2d9fc71203f0f93f4a14225a0020fa19
Samantha Divina <lavcorps@protonmail.com>
2026-02-13 17:28:53 -0700

    Mark monolog_player as whitespace sensitive

--- Commands/monolog_player.js
+++ Commands/monolog_player.js
@@ -14,7 +14,8 @@ export const config = {
 		+ `when it is sent in DMs. Please note that you cannot send a monolog that exceeds Discord's character limit, which is 2000 characters.`,
 	usableBy: "Player",
 	aliases: ["monolog", "monologue", "mo", "mn"],
-	requiresGame: true
+	requiresGame: true,
+	whitespaceSensitive: true
 };
 
 /**

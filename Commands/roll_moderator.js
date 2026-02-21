3780d9cd95c50202edc2dfa0730b0e2bff3c8251
Amy Poon <amy@amypoon.me>
2026-02-21 14:21:19 -0500

    Refactor Die class to TypeScript.

--- Commands/roll_moderator.js
+++ Commands/roll_moderator.js
@@ -1,4 +1,4 @@
-﻿import Die from '../Data/Die.js';
+﻿import Die from '../Data/Die.ts';
 import Player from '../Data/Player.js';
 
 /** @import GameSettings from '../Classes/GameSettings.js' */
@@ -21,8 +21,8 @@ export const config = {
 };
 
 /**
- * @param {GameSettings} settings 
- * @returns {string} 
+ * @param {GameSettings} settings
+ * @returns {string}
  */
 export function usage(settings) {
     return `${settings.commandPrefix}roll\n`
@@ -35,10 +35,10 @@ export function usage(settings) {
 }
 
 /**
- * @param {Game} game - The game in which the command is being executed. 
- * @param {UserMessage} message - The message in which the command was issued. 
- * @param {string} command - The command alias that was used. 
- * @param {string[]} args - A list of arguments passed to the command as individual words. 
+ * @param {Game} game - The game in which the command is being executed.
+ * @param {UserMessage} message - The message in which the command was issued.
+ * @param {string} command - The command alias that was used.
+ * @param {string[]} args - A list of arguments passed to the command as individual words.
  */
 export async function execute(game, message, command, args) {
     let statString = null, stat = null, attacker = null, defender = null;
--- Data/Die.ts
+++ Data/Die.ts
@@ -1,62 +1,52 @@
 ﻿import GameConstruct from './GameConstruct.ts';
 import Status from './Status.js';
-
-/** @import Game from './Game.js' */
-/** @import Player from './Player.js' */
+import type Game from "./Game.js";
+import type Player from "./Player.js";
 
 /**
- * @class Die
- * @classdesc Represents a die that can be rolled for a semi-random number.
- * @extends GameConstruct
+ * Represents a die that can be rolled for a semi-random number.
+ *
  * @see https://molsnoo.github.io/Alter-Ego/reference/data_structures/die.html
  */
 export default class Die extends GameConstruct {
     /**
      * The minimum possible base roll.
-     * @type {number}
      */
-    min;
+    min: number;
     /**
      * The maximum possible base roll.
-     * @type {number}
      */
-    max;
+    max: number;
     /**
      * The result of the die roll with no modifiers applied.
-     * @type {number}
      */
-    baseRoll;
+    baseRoll: number;
     /**
      * The calculated number to add to the baseRoll.
-     * @type {number}
      */
-    modifier;
+    modifier: number;
     /**
      * A string representation of all modifiers applied to the baseRoll, as well as the reasons they were applied.
-     * @type {string}
      */
-    modifierString;
+    modifierString: string;
     /**
      * The final result of the die roll.
-     * @type {number}
      */
-    result;
+    result: number;
 
     /**
-     * @constructor
-     * @param {Game} game - The game context this roll is occurring in. 
-     * @param {string} [stat] - The name of the stat to roll for. 
-     * @param {Player} [attacker] - The active player to roll for. In other words, the player attempting an action.
-     * @param {Player} [defender] - The passive player to roll for. Only used if the attacker is attempting to perform an action against another player.
+     * @param game - The game context this roll is occurring in.
+     * @param stat - The name of the stat to roll for.
+     * @param attacker - The active player to roll for. In other words, the player attempting an action.
+     * @param defender - The passive player to roll for. Only used if the attacker is attempting to perform an action against another player.
      */
-    constructor(game, stat, attacker, defender) {
+    constructor(game: Game, stat?: string, attacker?: Player, defender?: Player) {
         super(game);
 
         this.min = this.getGame().settings.diceMin;
         this.max = this.getGame().settings.diceMax;
 
-        /** @type {number} */
-        let baseRoll;
+        let baseRoll: number;
         if (attacker && attacker.hasBehaviorAttribute("all or nothing")) {
             // Make the base roll either the minimum or maximum possible.
             baseRoll = this.doBaseRoll(0, 1);
@@ -74,25 +64,25 @@ export default class Die extends GameConstruct {
 
     /**
      * Roll a die with no modifiers.
-     * @param {number} [min] - The minimum possible roll. Defaults to diceMin in the game's settings.
-     * @param {number} [max] - The maximum possible roll. Defaults to diceMax in the game's settings.
-     * @returns {number} A random number between min and max, inclusive.
+     *
+     * @param min - The minimum possible roll. Defaults to diceMin in the game's settings.
+     * @param max - The maximum possible roll. Defaults to diceMax in the game's settings.
+     * @returns A random number between min and max, inclusive.
      */
-    doBaseRoll(min = this.min, max = this.max) {
+    doBaseRoll(min: number = this.min, max: number = this.max): number {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
     /**
      * Calculates the modifiers to apply to the base roll.
-     * @param {string} [stat] - The stat which will modify the base roll.
-     * @param {Player} [attacker]
-     * @param {Player} [defender] 
-     * @returns {ModifierResult}
+     *
+     * @param stat - The stat which will modify the base roll.
+     * @param attacker - The active player to roll for. In other words, the player attempting an action.
+     * @param defender - The passive player to roll for. Only used if the attacker is attempting to perform an action against another player.
      */
-    calculateModifiers(stat, attacker, defender) {
+    calculateModifiers(stat?: string, attacker?: Player, defender?: Player): ModifierResult {
         let modifier = 0;
-        /** @type {string[]} */
-        let modifierStrings = [];
+        let modifierStrings: string[] = [];
         if (attacker) {
             if (attacker.hasBehaviorAttribute("coin flipper")) {
                 let hasCoin = false;
@@ -111,8 +101,7 @@ export default class Die extends GameConstruct {
                 }
             }
 
-            /** @type {Status[]} */
-            let tempStatuses = [];
+            let tempStatuses: Status[] = [];
             if (defender) {
                 // If the attacker is attacking the defender with their strength, use the defender's dexterity stat determines how well they dodged it.
                 if (stat === "str") {
